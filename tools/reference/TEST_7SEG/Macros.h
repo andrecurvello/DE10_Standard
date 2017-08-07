86148018644f9f6daf4fe190c746c24a692a48a6
badiss.djafar <badiss.djafar@gmail.com>
2017-08-07 11:49:07 +0200

    Fixing the merge mayhem

---
+++ tools/reference/TEST_7SEG/Macros.h
@@ -0,0 +1,40 @@
+/* *****************************************************************************
+**
+**                       General macros definitions
+**
+** Project      : -
+** Module       : Macros.h
+**
+** Description  : This is the legacy header file that encapsulate useful macros
+**
+** ************************************************************************** */
+
+/* *****************************************************************************
+**  REVISION HISTORY
+** Date         Inits   Description
+** ----------------------------------------------------------------------------
+** 28.05.17     bd      [no issue number] File creation
+**
+** ************************************************************************** */
+
+#define BYTE_MAX (0xFF)
+#define DWORD_MAX (0xFFFF)
+#define QWORD_MAX (0xFFFFFFFF)
+
+#if 0
+#define mSetBit_BYTE()
+#define mSetBit_DWORD()
+#define mSetBit_QWORD()
+#endif
+
+#ifndef TRUE
+#define TRUE (1)
+#endif
+
+#ifndef FALSE
+#define FALSE (0)
+#endif
+
+#ifndef NULL
+#define NULL (0x00000000) /* Null pointer definition */
+#endif
